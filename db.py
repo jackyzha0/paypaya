b780a1c30adf58a169dd1c70b86005b8b9f9d65f
jackyzha0 <j.zhao2k19@gmail.com>
2020-11-21 10:54:30 -0800

    add base onboarding flow and db modularization

--- db.py
+++ db.py
@@ -1,17 +1,32 @@
 from pymongo import MongoClient
+class Db:
+    def __init__(self, connection_url, database_name):
+        self.client = MongoClient(connection_url)
+        self.db = self.client.get_database(database_name)
+        self.records = self.db.user_records
+    
+    def new_user(self, user):
+        self.records.insert_one(user.__dict__)
+        print(f"successfully inserted new user {user.phone}")
+    
+    def get_user(self, phone):
+        return self.records.find_one({"phone": phone})
 
-client = MongoClient('mongodb+srv://paypaya-main:test@cluster0.5hure.mongodb.net/paypaya_db?retryWrites=true&w=majority')
-db = client.get_database('paypaya_db')
-records = db.user_records
+    def update_user(self, filter, update_value):
+        self.records.update_one(filter, {'$set': update_value})
 
-//inserting new users
-new_user = {
-    "paypal_email": "addingtest@test.com",
-    "name": "test adding name",
-    "voice_identity":"test identity test"
-}
+class User:
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+        self.paypal_email = ""
+        self.voice_identity = ""
+        self.onboarding_status = 0
 
-records.insert_one(new_user)
+db = Db( 'mongodb+srv://paypaya-main:test@cluster0.5hure.mongodb.net/paypaya_db?retryWrites=true&w=majority', 'paypaya_db')
 
-//find documents
-list(records.find())
+
+# new_user = User("name test", "phone test")
+# db.new_user(new_user)
+# print(db.get_user("phone test"))
+# db.update_user({"name": "name test"}, {"name": 'yeet'})
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 import os
+from db import *
 
 app = Flask(__name__)
 
@@ -17,13 +18,39 @@ def incoming_sms():
     resp = MessagingResponse()
 
     # Determine the right reply for this message
-    if body == 'hello':
-        resp.message("Hi!")
-    elif body == 'bye':
-        resp.message("Goodbye")
+    resp.message(handle_sms(sender, body))
 
     return str(resp)
 
+default_help_str = """Sorry, I couldn't understand what you were trying to do. Here are some of the available commands:
+    send $<amount> <phone-number>
+    balance
+    ...
+"""
+
+def handle_sms(sender_number, body):
+    # fetch user info from mongo using sender number
+    user_info = db.get_user(sender)
+
+    # not in database
+    if not user_info:
+        # onboarding flow step -1
+        # create user in database
+        print(f"new user, adding {sender_number} to database")
+        new_user = User("placeholder_name", sender_number)
+        db.new_user(new_user)
+
+        return "Hi there! Thanks for signing up for Paypaya. Please respond with your name to complete the setup."
+
+    # check onboarding status
+    if user_info.onboarding_status == 0:
+        # onboarding flow step 1
+        # fill in name
+        db.update_user({"phone": sender_number}, {"name": body})
+        return f"Thanks, ${body}! You're good to go!"
+
+    # unknown status/command, return default_help_str
+    return default_help_str
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
